49d3349493c3820d154034cdc93a0a0b2391cdd3
Mike <penskimike@gmail.com>
2021-03-06 10:54:05 +0100

    update login

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,6 @@
 "use strict";
 
+//no need anymore just for look in data
 const USERS = [ 
     { name: "supercode", secret: "no_one_will_know" },
     { name: "music_fan_1990", secret: "WeAreTheChampi0ns" },
@@ -7,16 +8,19 @@ const USERS = [
 ];
 
 
-
+//vars
 const body = document.querySelector("body");
 const loginModal = document.getElementById("loginModal");
 const loginForm = document.getElementById("loginForm");
 const loginUserName = document.getElementById("username");
-//const loginPassword = document.getElementById("password");
+const loginPassword = document.getElementById("password");
 const loginModalSubmit = document.getElementById("submit");
 const logoutLink = document.getElementById("log-out");
 const usernameContainer = document.querySelector(".username");
 
+
+//set focus to login username input
+loginUserName.focus();
 
 /*
 * cookie functions
@@ -38,33 +42,31 @@ function deleteCookie(name) {
 }
 
 /*
-* check user login input
+* print error messsage
 */
-let errorMessage = (showmessage) => {
+let printError = (message) => {
 
-    if(showmessage == true){
-
-            //create errorMessage and class
-            const errorContainer = document.getElementById("errorMessage");   
-            if(errorContainer == null){
-                let errorMessage = document.createElement("div");
-                errorMessage.id = "errorMessage";
-                errorMessage.innerHTML = "*user does not exist.";
-                loginForm.appendChild(errorMessage);
-            }
-            loginUserName.classList.add("alert");
-
-    } else {
-
-          //remove errorMessage and class
-          let errorContainer = document.getElementById("errorMessage"); 
-          if(errorContainer != null){
-              errorContainer.parentNode.removeChild(errorContainer);
-          }
-          loginUserName.classList.remove("alert");
-          
+    //create errorMessage and class
+    const errorContainer = document.getElementById("errorMessage");   
+    if(errorContainer == null){
+        let errorMessage = document.createElement("div");
+        errorMessage.id = "errorMessage";
+        loginForm.appendChild(errorMessage);
     }
 
+    //check if user is found
+    if(message == "user not found"){
+        console.log(errorMessage)
+        errorMessage.innerHTML = "*user does not exist.";
+        loginUserName.classList.add("alert");
+    }
+
+    //check if password is correct
+    if(message == "wrong password"){
+        console.log(errorMessage)
+        errorMessage.innerHTML = "*wrong password.";
+        loginPassword.classList.add("alert");
+    }
 }
 
 /*
@@ -74,6 +76,50 @@ let welcomeUser = (userName) => {
     body.classList.remove("modalOpen");
     loginModal.classList.add("d-none");
     usernameContainer.innerHTML = userName;
+}
+
+/*
+* check user is logged in
+*/
+let checkUserisLoggedin = () => {
+
+    let myCookie = getCookie('userLogin');
+
+    console.log("teeeest " + myCookie)
+
+    if(typeof myCookie !== 'undefined'){
+
+        myCookie = JSON.parse(myCookie);
+
+        if (1 === myCookie.value) {
+            //get username and call weclome user function
+            const userName = myCookie.username;
+            console.log(userName)
+            welcomeUser(userName);
+        }
+
+    }
+
+   
+}
+
+/*
+*create cookie
+*/
+
+let createCookie = (userName) => {
+
+    let cookieValue = {
+        value: 1, username: userName
+    }
+
+    cookieValue = JSON.stringify(cookieValue);
+  
+    setCookie("userLogin", cookieValue, 365);
+
+    if(typeof myCookie == 'undefined'){
+        checkUserisLoggedin();
+    }
 
 }
 
@@ -85,47 +131,40 @@ let checkLogin = () => {
     let userInputName = document.getElementById("username").value.toLowerCase();
     let userINputPassword = document.getElementById("password").value;
 
-    const user = USERS.find(item => item.name === userInputName);
+    const data = {
+        name: userInputName,
+        secret: userINputPassword
+    };
 
-        if(user && user.name === userInputName){
+    fetch('https://supercode-auth-demo.herokuapp.com/', {
+    method: 'POST', // or 'PUT'
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+    })
+    .then(response => response.json())
+    .then(data => {
 
-            errorMessage(false)
+    //console.log('Success:', data);
 
-            if(userINputPassword == user.secret){
+    if(data.success == true){
+        console.log("logged in");
+        createCookie(data.user);
+    } 
 
-                let cookieValue = {
-                    value: 1, username: user.name
-                }
-                cookieValue = JSON.stringify(cookieValue);
-   
-                setCookie("userLogin", cookieValue, 365);
+    if(data.success == false){
+        console.log("false");
+        printError(data.message);
+    } 
 
-                let myCookie = getCookie('userLogin');
+    })
+    
+    .catch((error) => {
+        console.error('Error:', error);
+    });
 
-                if(typeof myCookie !== 'undefined'){
 
-                    checkUserisLoggedin();
-
-                }
-
-            }
-
-        } else {
-            errorMessage(true);
-        }
-}
-
-/*
-* check user is logged in
-*/
-let checkUserisLoggedin = () => {
-    myCookie = decodeURIComponent(myCookie);
-    myCookie = JSON.parse(myCookie);
-    if (1 === myCookie.value) {
-        //get username and call weclome user function
-        const userName = myCookie.username;
-        welcomeUser(userName);
-    }
 }
 
 
@@ -133,10 +172,8 @@ let checkUserisLoggedin = () => {
 * check if cookie is set and call checkUserisLoggedin function
 */
 
-let myCookie = getCookie('userLogin');
-if(typeof myCookie !== 'undefined'){
-    checkUserisLoggedin();
-}
+checkUserisLoggedin();
+
 
 /*
 * event listener for login form
@@ -156,4 +193,6 @@ logoutLink.addEventListener("click", (e) => {
 })
 
 
-
+    
+ 
+    
